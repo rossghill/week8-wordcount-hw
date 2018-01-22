43df9abd1492a7c0f0f5be103a07ff5b2bd57016
Ross Hill <rosshill88@gmail.com>
2018-01-22 19:50:55 +0000

    Button tested returning string with Logcat

--- app/src/main/java/com/example/rosshill/wordcounter/WordCountActivity.java
+++ app/src/main/java/com/example/rosshill/wordcounter/WordCountActivity.java
@@ -2,12 +2,30 @@ package com.example.rosshill.wordcounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class WordCountActivity extends AppCompatActivity {
+
+    EditText editText;
+    Button countButton;
+    TextView resultCount;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editText = findViewById(R.id.editText);
+        countButton = findViewById(R.id.button);
+        resultCount = findViewById(R.id.textView4);
+    }
+
+    public void onCountButtonClicked(View button){
+        String edit_text = editText.getText().toString();
+        Log.d(getClass().toString(), "Button clicked:");
     }
 }
